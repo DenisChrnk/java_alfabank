77e2acc1127af25da01e4e8d77db8ba691aaa730
DenisChrnk <denis.chakchak@gmail.com>
2026-05-01 21:47:00 +0300

    Добавил задание 3 и 4

--- src/main/java/org/example/homework3/Homework3Arrays.java
+++ src/main/java/org/example/homework3/Homework3Arrays.java
@@ -15,40 +15,68 @@ public class Homework3Arrays {
         // Устанавливаем UTF-8 для вывода ошибок (System.err)
         System.setErr(new PrintStream(new FileOutputStream(FileDescriptor.err), true, StandardCharsets.UTF_8));
 
-        String [] petya = {"курица", "бананы", "творого"};
-        String [] kolya = {"курица", "бананы", "творого"};
-        String [] terentiy = {"пиво", "пельмени", "ласка магия черного"};
+        String[] petya = {"курица", "бананы", "творого"};
+        String[] kolya = {"курица", "бананы", "творого"};
+        String[] terentiy = {"пиво", "пельмени", "ласка магия черного"};
 
+        //Сравнение ро количеству
+        boolean differenceSizeBetweenPetyaAndKolya = petya.length == kolya.length;
+        boolean differenceSizeBetweenPetyaAndTerentiy = petya.length == terentiy.length;
+
+        System.out.println("Разное количество товаров в корзине у Пети и Коли: " + differenceSizeBetweenPetyaAndKolya);
+        System.out.println("Разное количество товаров в корзине у Пети и Терентия: " + differenceSizeBetweenPetyaAndTerentiy);
+
+        //Сравнение по содержимому
         boolean differenceBetweenPetyaAndKolya = Arrays.equals(petya, kolya);
         boolean differenceBetweenPetyaAndTerentiy = Arrays.equals(petya, terentiy);
 
-        System.out.println("Совпадение корзины Пети и Коли - " + differenceBetweenPetyaAndKolya);
-        System.out.println("Совпадение корзины Пети и Терентия - " + differenceBetweenPetyaAndTerentiy);
+        System.out.println("Совпадение товаров в корзине Пети и Коли - " + differenceBetweenPetyaAndKolya);
+        System.out.println("Совпадение товаров в корзине Пети и Терентия - " + differenceBetweenPetyaAndTerentiy);
 
         //Задание с самым длинным словом
-
         ArrayList<String> allWords = new ArrayList<>();
 
-        for (String word: petya){
+        for (String word : petya) {
             allWords.add(word);
         }
 
-        for (String word: kolya){
+        for (String word : kolya) {
             allWords.add(word);
         }
 
-        for (String word: terentiy){
+        for (String word : terentiy) {
             allWords.add(word);
         }
 
         String longestWord = "";
 
-        for (String wordForComparison: allWords){
-            if (wordForComparison.length() > longestWord.length()){
+        for (String wordForComparison : allWords) {
+            if (wordForComparison.length() > longestWord.length()) {
                 longestWord = wordForComparison;
             }
         }
 
         System.out.println("Самое длинное слово: " + longestWord);
+
+        //Самое короткое слово
+
+        String shortestWord = longestWord;
+
+        for (String wordForComparison : allWords) {
+            if (wordForComparison.length() < shortestWord.length()) {
+                shortestWord = wordForComparison;
+            }
+        }
+
+        System.out.println("Самое короткое слово: " + shortestWord);
+
+        //Средняя длина названия товара
+        StringBuilder allStrings = new StringBuilder();
+        for (String wordForComparation : allWords){
+            allStrings.append(wordForComparation);
+        }
+        double intermediateSize = (double) allStrings.length()/allWords.size();
+
+        System.out.println("Средняя длина слова: " + intermediateSize);
     }
 }
