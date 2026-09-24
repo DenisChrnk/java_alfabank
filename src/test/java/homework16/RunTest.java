9c1995f55180a5784fa88ed6af982c64dba1bc22
DenisChrnk <denis.chakchak@gmail.com>
2026-09-24 11:49:55 +0300

    Домашнее задание 16

---
+++ src/test/java/homework16/RunTest.java
@@ -0,0 +1,20 @@
+package homework16;
+
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+//@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@regression")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "homework16.steps")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:build/cucumber-report/report.html")
+
+public class RunTest {
+}
---
+++ src/test/java/homework16/hooks/Hooks.java
@@ -0,0 +1,21 @@
+package homework16.hooks;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class Hooks {
+    @Before
+    public void before(Scenario scenario) {
+        System.out.println("Начинается сценарий: " + scenario.getName());
+    }
+
+    @After
+    public void after(Scenario scenario) {
+        if (scenario.isFailed()) {
+            System.out.println("Сценарий " + scenario.getName() + " упал");
+        } else {
+            System.out.println("Сценарий " + scenario.getName() + " прошел успешно");
+        }
+    }
+}
